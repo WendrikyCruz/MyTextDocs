20101654184550821fe829bddfe767a84f852bd0
Wendriky <wendriky.cruz@outlook.com>
2020-07-22 22:20:38 -0300

    Dado inicio ao projeto MyTextDocs

---
+++ src/test/java/com/MyTextDocs/MyTextDocs/MyTextDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyTextDocs.MyTextDocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTextDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
