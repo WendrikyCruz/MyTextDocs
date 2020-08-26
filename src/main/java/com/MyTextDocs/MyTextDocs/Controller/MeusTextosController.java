ae577091f2b2db7dcc285efa5dfc9e62cc4c166a
Wendriky <wendriky.cruz@outlook.com>
2020-08-26 06:35:55 -0300

    revisao v1.1

--- src/main/java/com/MyTextDocs/MyTextDocs/Controller/MeusTextosController.java
+++ src/main/java/com/MyTextDocs/MyTextDocs/Controller/MeusTextosController.java
@@ -24,14 +24,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Optional;
+
 
 
 @Controller
@@ -122,6 +118,7 @@ public class MeusTextosController {
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.add("Content-Disposition", "attachment;filename=\""+texto.getNome()+".pdf\"");
             HttpEntity<byte[]> entity = new HttpEntity<byte[]>( arquivo, httpHeaders);
+            file.delete();
             return entity;
         }
         return null;
--- src/main/java/com/MyTextDocs/MyTextDocs/MyTextDocsApplication.java
+++ src/main/java/com/MyTextDocs/MyTextDocs/MyTextDocsApplication.java
@@ -9,7 +9,6 @@ public class MyTextDocsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyTextDocsApplication.class, args);
-
 	}
 
 }
