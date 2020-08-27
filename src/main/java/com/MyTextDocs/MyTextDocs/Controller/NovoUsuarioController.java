5dd77ba65a7886e79759052cf2049d67a14e0346
Wendriky <wendriky.cruz@outlook.com>
2020-08-27 07:24:50 -0300

    Atualizado conexão banco heroku v1.3

--- src/main/java/com/MyTextDocs/MyTextDocs/Controller/NovoUsuarioController.java
+++ src/main/java/com/MyTextDocs/MyTextDocs/Controller/NovoUsuarioController.java
@@ -4,6 +4,7 @@ import com.MyTextDocs.MyTextDocs.Models.Usuario;
 import com.MyTextDocs.MyTextDocs.Services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -12,6 +13,11 @@ public class NovoUsuarioController {
 
     @Autowired
     UsuarioService usuarioService;
+
+    @GetMapping("/NovoUsuario")
+    public String novoUsuarioPage(){
+        return "/NovoUsuario";
+    }
 
     @PostMapping("/NovoUsuario")
     public String NovoUsuario(Usuario usuario, RedirectAttributes redirectAttributes){
